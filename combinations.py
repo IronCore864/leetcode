09a49437f4820e09a4ccb12496c19ddadde3e0a5
Tiexin Guo <tiexin.guo@kuehne-nagel.com>
2017-01-10 12:16:12 +0200

    77. Combinations

---
+++ combinations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combine(self, n, k):
+        if k == 1:
+            return [[i] for i in range(1, n + 1)]
+        elif k == n:
+            return [[i for i in range(1, n + 1)]]
+        else:
+            rs = []
+            rs += self.combine(n - 1, k)
+            part = self.combine(n - 1, k - 1)
+            for ls in part:
+                ls.append(n)
+            rs += part
+            return rs
+
+
+s = Solution()
+res = s.combine(10, 3)
+for row in res:
+    print row
