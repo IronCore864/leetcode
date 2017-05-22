5868c7dd73da11c9fc1dc61c2c1daeb902117fd7
Tiexin Guo <guotiexin@gmail.com>
2017-05-22 15:52:26 +0300

    155. Min Stack

---
+++ 1/min_stack.py
@@ -0,0 +1,39 @@
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.q = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if len(self.q) == 0:
+            self.q.append((x, x))
+        else:
+            self.q.append((x, min(x, self.getMin())))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.q.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.q) == 0:
+            return None
+        return self.q[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if len(self.q) == 0:
+            return None
+        else:
+            return self.q[-1][1]
