497720fa957b6b841a3fab9837bf15d9e99c5ebc
Tiexin Guo <guotiexin@gmail.com>
2017-06-20 10:51:07 +0300

    204. Count Primes

---
+++ 2/count_primes.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def countPrimes(self, n):
+        if n < 3:
+            return 0
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
+        return sum(primes)
