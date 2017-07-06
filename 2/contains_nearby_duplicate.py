c3fc35da1ab9d741ff50f72bc8dd2719190ec04f
Tiexin Guo <guotiexin@gmail.com>
2017-07-06 10:40:44 +0300

    219. Contains Duplicate II

---
+++ 2/contains_nearby_duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dic = {}
+        for i, v in enumerate(nums):
+            if v in dic and i - dic[v] <= k:
+                return True
+            dic[v] = i
+        return False
