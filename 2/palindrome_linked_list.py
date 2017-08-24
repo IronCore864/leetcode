48e72762f57e59a6f74213673c521cbb79dc78f1
Tiexin Guo <guotiexin@gmail.com>
2017-08-24 12:11:40 +0300

    234. Palindrome Linked List

---
+++ 2/palindrome_linked_list.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        rev = None
+        slow = fast = head
+        # reverse first half and compare the two halves
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+        return not rev
