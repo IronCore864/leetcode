c562cc3d24461853fe4f74c20beec09bcc02ebbf
Tiexin Guo <guotiexin@gmail.com>
2017-09-14 15:48:07 +0300

    306. Additive Number

---
+++ 3/additive_number.py
@@ -0,0 +1,30 @@
+from itertools import combinations
+
+
+class Solution(object):
+    def isAdditiveNumber(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+
+        n = len(num)
+        for i, j in combinations(range(1, n), 2):
+            a, b = num[:i], num[i:j]
+
+            if a != str(int(a)) or b != str(int(b)):
+                continue
+
+            while j < n:
+                c = str(int(a) + int(b))
+
+                if not num.startswith(c, j):
+                    break
+
+                j += len(c)
+                a, b = b, c
+
+            if j == n:
+                return True
+
+        return False
