ef3088fb94250287e3aea06adde60ba3a0482639
Tiexin Guo <guotiexin@gmail.com>
2017-09-20 10:32:51 +0300

    310. Minimum Height Trees

---
+++ 3/min_height_trees.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        if n == 1:
+            return [0]
+
+        adj = [set() for _ in range(n)]
+
+        for i, j in edges:
+            adj[i].add(j)
+            adj[j].add(i)
+
+        leaves = [i for i in range(n) if len(adj[i]) == 1]
+
+        while n > 2:
+            n -= len(leaves)
+            newLeaves = []
+            for i in leaves:
+                j = adj[i].pop()
+                adj[j].remove(i)
+                if len(adj[j]) == 1:
+                    newLeaves.append(j)
+            leaves = newLeaves
+
+        return leaves
