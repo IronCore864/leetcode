5df56df56ad707c503b1dc9409d86105f814257a
Tiexin Guo <guotiexin@gmail.com>
2018-01-02 14:22:06 +0200

    521. Longest Uncommon Subsequence I

---
+++ 5/longest_uncommon_subsequence1.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        return -1 if a == b else max(len(a), len(b))
+        
