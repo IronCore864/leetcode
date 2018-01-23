cb3326c60b309eb204b46c1c010cedbe71b40f7c
Tiexin Guo <Tiexin.Guo@myjar.com>
2018-01-23 16:06:01 +0200

    543. Diameter of Binary Tree, go

---
+++ 5/diameter_of_tree.go
@@ -0,0 +1,44 @@
+package main
+
+import ( 
+    "fmt"
+)
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func Max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
+
+func depth(root *TreeNode, maxlen *int) int {
+    if root == nil {
+        return 0
+    }
+    var l = depth(root.Left, maxlen)
+    var r = depth(root.Right, maxlen)
+    *maxlen = Max(l + r + 1, *maxlen)
+    return Max(l, r) + 1
+}
+
+func diameterOfBinaryTree(root *TreeNode) int {
+    var maxlen = 0
+    if root==nil {
+        return 0
+    }
+    depth(root, &maxlen)
+    fmt.Println(maxlen - 1)
+    return maxlen - 1
+}
+
+func main() {
+    var root = TreeNode{1, nil, nil}
+    diameterOfBinaryTree(&root)
+}
+
