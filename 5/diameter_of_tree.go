47786e4d0f956ef96f747785720035aabb612e6c
Tiexin Guo <Tiexin.Guo@myjar.com>
2018-01-24 15:41:24 +0200

    reformat and refactor go var name

--- 5/diameter_of_tree.go
+++ 5/diameter_of_tree.go
@@ -1,44 +1,43 @@
 package main
 
-import ( 
-    "fmt"
+import (
+	"fmt"
 )
 
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 func Max(x, y int) int {
-    if x > y {
-        return x
-    }
-    return y
+	if x > y {
+		return x
+	}
+	return y
 }
 
 func depth(root *TreeNode, maxlen *int) int {
-    if root == nil {
-        return 0
-    }
-    var l = depth(root.Left, maxlen)
-    var r = depth(root.Right, maxlen)
-    *maxlen = Max(l + r + 1, *maxlen)
-    return Max(l, r) + 1
+	if root == nil {
+		return 0
+	}
+	var l = depth(root.Left, maxlen)
+	var r = depth(root.Right, maxlen)
+	*maxlen = Max(l+r+1, *maxlen)
+	return Max(l, r) + 1
 }
 
 func diameterOfBinaryTree(root *TreeNode) int {
-    var maxlen = 0
-    if root==nil {
-        return 0
-    }
-    depth(root, &maxlen)
-    fmt.Println(maxlen - 1)
-    return maxlen - 1
+	var maxlen = 0
+	if root == nil {
+		return 0
+	}
+	depth(root, &maxlen)
+	fmt.Println(maxlen - 1)
+	return maxlen - 1
 }
 
 func main() {
-    var root = TreeNode{1, nil, nil}
-    diameterOfBinaryTree(&root)
+	var root = TreeNode{1, nil, nil}
+	diameterOfBinaryTree(&root)
 }
-
--- 5/student_attendance_record.go
+++ 5/student_attendance_record.go
@@ -4,24 +4,24 @@ import "fmt"
 
 func checkRecord(s string) bool {
 	absent := 0
-	continuous_late := 0
+	continuousLate := 0
 
 	runes := []rune(s)
 	for i := 0; i < len(runes); i++ {
 		switch {
 		case runes[i] == 'A':
 			absent += 1
-			continuous_late = 0
+			continuousLate = 0
 			if absent > 1 {
 				return false
 			}
 		case runes[i] == 'L':
-			continuous_late += 1
-			if continuous_late > 2 {
+			continuousLate += 1
+			if continuousLate > 2 {
 				return false
 			}
 		case runes[i] == 'P':
-			continuous_late = 0
+			continuousLate = 0
 		}
 	}
 	return true
