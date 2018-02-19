5263b258e609924148b5299dc288ebf8b2323d21
Tiexin Guo <Tiexin.Guo@myjar.com>
2018-02-19 11:51:42 +0200

    316. Remove Duplicate Letters

---
+++ 3/remove_duplicate_letters.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeDuplicateLetters(self, s):
+        for c in sorted(set(s)):
+            suffix = s[s.index(c):]
+            if set(suffix) == set(s):
+                return c + self.removeDuplicateLetters(suffix.replace(c, ''))
+        return ''
