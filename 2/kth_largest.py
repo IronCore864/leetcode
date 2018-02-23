1a77caa388609b0755c5fa339f0e1c8ad0dd32e7
Tiexin Guo <Tiexin.Guo@myjar.com>
2018-02-23 12:36:13 +0200

    215. Kth Largest Element in an Array

--- 2/kth_largest.py
+++ 2/kth_largest.py
@@ -1,16 +1,30 @@
-from heapq import nlargest, heapify
-
-
-class Solution(object):
+class Solution:
     def findKthLargest(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
         """
-        heapify(nums)
-        return nlargest(k, nums)[-1]
+        return self.partition(nums, 0, len(nums) - 1, k)
+
+    def partition(self, nums, left, right, k):
+        pivot = nums[right]
+        i = left
+        for j in range(left, right):
+            if nums[j] > pivot:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        nums[i], nums[right] = nums[right], nums[i]
+
+        if i + 1 == k:
+            return nums[i]
+        elif i + 1 > k:
+            return self.partition(nums, left, i - 1, k)
+        else:
+            return self.partition(nums, i, right, k)
 
 
 s = Solution()
-print(s.findKthLargest([3, 2, 1, 5, 6, 4], 2))
+a = [6, 2, 5, 3, 1, 4]
+for i in range(1, len(a) + 1):
+    print(s.findKthLargest(a, i))
