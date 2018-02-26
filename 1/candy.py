b08c4235188f4929df364a90c4a8ec98b4bebb32
Tiexin Guo <Tiexin.Guo@myjar.com>
2018-02-26 11:22:14 +0200

    135. Candy

--- 1/candy.py
+++ 1/candy.py
@@ -4,37 +4,22 @@ class Solution(object):
         :type ratings: List[int]
         :rtype: int
         """
-        total = 1
-        prev = 1
-        count = 0
+        if not ratings:
+            return 0
 
-        for i in xrange(1, len(ratings)):
-            if ratings[i] >= ratings[i - 1]:
-                if count > 0:
-                    total += (1 + count) * count / 2
-                    if count >= prev:
-                        total += count - prev + 1
-                    count = 0
-                    prev = 1
-                prev = 1 if ratings[i] == ratings[i - 1] else prev + 1
-                total += prev
-            else:
-                count += 1
-        if count > 0:
-            total += (1 + count) * count / 2
-            if count >= prev:
-                total += count - prev + 1
-        return total
+        n = len(ratings)
+        candies = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], (candies[i + 1] + 1))
+
+        return sum(candies)
 
 
 s = Solution()
-print s.candy([1, 2, 2])
-print s.candy([2, 2])
-print s.candy([2, 3, 2])
-print s.candy([8, 9, 7, 2])
-print s.candy([4, 3, 2, 1])
-print s.candy([8, 9, 7, 2, 10, 6, 3, 1, 4, 5])
-print s.candy([10, 6, 3, 1, 4, 5])
-print s.candy([1, 0, 2])
-print s.candy([4, 3, 2, 2])
-print s.candy([1, 2, 4, 4, 3])
+print(s.candy([1, 2, 2]))
+print(s.candy([2, 2, 1]))
