ec620de6e53205efbe35df6535ece4e5d2a4b46a
Tiexin Guo <guotiexin@gmail.com>
2019-11-08 12:41:37 +0100

    O(1) solution without proof 343. Integer Break

--- 3/integer_break.go
+++ 3/integer_break.go
@@ -28,9 +28,35 @@ func integerBreak(n int) int {
 	return dp[len(dp)-1]
 }
 
+// O(1) solution without proof
+// https://leetcode.com/problems/integer-break/discuss/423970/O(1)-Solution
+func test(n int) int {
+	if n == 2 {
+		return 1
+	}
+	if n == 3 {
+		return 2
+	}
+	a := n / 3
+	b := n % 3
+	res := 1
+	switch b {
+	case 0:
+		res = int(math.Pow(3, float64(a)))
+	case 1:
+		res = int(math.Pow(3, float64(a-1))) * 4
+	case 2:
+		res = int(math.Pow(3, float64(a))) * 2
+	}
+	return res
+}
+
+
 func main() {
 	fmt.Println(integerBreak(3))
 	fmt.Println(integerBreak(8))
+	fmt.Println(test(8))
 	fmt.Println(integerBreak(58))
+	fmt.Println(test(58))
 }
 
