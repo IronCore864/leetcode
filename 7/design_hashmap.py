5fae9df7630a12104a5db4755f6a36108d5c6864
Tiexin Guo <guotiexin@gmail.com>
2020-01-02 10:49:35 +0100

    Update design_hashmap.py

--- 7/design_hashmap.py
+++ 7/design_hashmap.py
@@ -24,7 +24,7 @@ class MyHashMap:
                 self.used += 1
                 current = current.next
         self.buckets = new_buckets
-        print("Resizing: current size: {} current used: {} new size {}".format(self.size // 2, self.used, self.size))
+        # print("Resizing: current size: {} current used: {} new size {}".format(self.size // 2, self.used, self.size))
 
     def _check_capacity_and_resize(self):
         capacity = self.used / self.size
@@ -45,14 +45,14 @@ class MyHashMap:
     def put(self, key: int, value: int) -> None:
         self._check_capacity_and_resize()
         previous, current = self._find_node_return_previous_and_current(key)
-        if not previous:
+        if current is None and previous is None:
             self.buckets[self._hash(key)] = ListNode(key, value)
             self.used += 1
-        elif not current:
+        elif previous is not None and current is not None:
+            current.val = value
+        elif previous is not None and current is None:
             previous.next = ListNode(key, value)
             self.used += 1
-        elif current.key == key:
-            current.val = value
 
     def get(self, key: int) -> int:
         previous, current = self._find_node_return_previous_and_current(key)
@@ -60,15 +60,42 @@ class MyHashMap:
 
     def remove(self, key: int) -> None:
         previous, current = self._find_node_return_previous_and_current(key)
-        if not previous:
+        if current is None:
             return
         elif previous == current:
-            self.buckets[self._hash(key)] = None
-            self.used -= 1
-        elif current is not None:
+            self.buckets[self._hash(key)] = current.next
+        else:
             previous.next = current.next
             self.used -= 1
 
+
+op = ["MyHashMap", "put", "put", "put", "remove", "get", "put", "put", "get", "put", "put", "put", "put", "put", "put",
+      "put", "put", "put", "put", "put", "put", "remove", "put", "remove", "put", "put", "remove", "put", "get", "put",
+      "get", "put", "put", "put", "put", "put", "get", "put", "remove", "put", "remove", "put", "put", "put", "put",
+      "put", "remove", "put", "put", "remove", "put", "put", "put", "get", "get", "put", "remove", "put", "put", "put",
+      "get", "put", "put", "put", "remove", "put", "put", "put", "put", "put", "get", "put", "put", "get", "get", "put",
+      "remove", "remove", "get", "put", "remove", "put", "remove", "put", "put", "put", "get", "put", "put", "put",
+      "remove", "put", "put", "get", "put", "put", "get", "remove", "get", "get", "put"]
+param = [[], [24, 31], [58, 35], [59, 88], [84], [62], [2, 22], [44, 70], [24], [24, 42], [58, 99], [74, 29], [40, 66],
+         [55, 83], [21, 27], [31, 25], [78, 19], [86, 70], [71, 73], [39, 95], [6, 96], [76], [62, 22], [78], [53, 51],
+         [66, 53], [44], [14, 46], [77], [15, 32], [22], [53, 79], [35, 21], [73, 57], [18, 67], [96, 61], [73],
+         [58, 77], [6], [5, 58], [17], [25, 14], [16, 13], [4, 37], [47, 43], [14, 79], [35], [7, 13], [78, 85], [27],
+         [73, 33], [95, 87], [31, 21], [20], [64], [90, 22], [16], [77, 50], [55, 41], [33, 62], [44], [73, 16],
+         [13, 54], [41, 5], [71], [81, 6], [20, 98], [35, 64], [15, 35], [74, 31], [90], [32, 15], [44, 79], [37], [53],
+         [22, 80], [24], [10], [7], [53, 61], [65], [63, 99], [47], [97, 68], [7, 0], [9, 25], [97], [93, 13], [92, 43],
+         [83, 73], [74], [41, 78], [39, 28], [52], [34, 16], [93, 63], [82], [77], [16], [50], [68, 47]]
+
+hm = MyHashMap()
+for i in range(len(op)):
+    if op[i] == "MyHashMap":
+        hm = MyHashMap()
+        continue
+    if op[i] == "put":
+        print(hm.put(param[i][0], param[i][1]))
+    if op[i] == "get":
+        print(hm.get(param[i][0]))
+    if op[i] == "remove":
+        print(hm.remove(param[i][0]))
 
 print("simple test")
 hm = MyHashMap()
