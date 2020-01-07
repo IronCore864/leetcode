e9e1bc86384f9936839b26d8b1e0aa8cde2b9b5b
Tiexin Guo <guotiexin@gmail.com>
2020-01-07 11:32:55 +0100

    one liner

--- 7/self_dividing_num.py
+++ 7/self_dividing_num.py
@@ -1,16 +1,3 @@
 class Solution:
     def selfDividingNumbers(self, left, right):
-        res = []
-        for num in range(left, right + 1):
-            num_str = str(num)
-            r = True
-            for s in num_str:
-                if s == '0':
-                    r = False
-                    break
-                if num % int(s) != 0:
-                    r = False
-                    break
-            if r:
-                res.append(num)
-        return res
+        return [num for num in range(left, right + 1) if all(int(i) != 0 and num % int(i) == 0 for i in str(num))]
