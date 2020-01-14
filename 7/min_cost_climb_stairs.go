d5d32d1c6fec58993d021d65e5e55824c0a97e68
Tiexin Guo <guotiexin@gmail.com>
2020-01-14 15:54:09 +0100

    746. Min Cost Climbing Stairs - Golang

---
+++ 7/min_cost_climb_stairs.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost)+1)
+	for i := 2; i < len(dp); i++ {
+		dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+	return dp[len(dp)-1]
+}
+
+func main() {
+	fmt.Println(minCostClimbingStairs([]int{10, 15, 20}))
+	fmt.Println(minCostClimbingStairs([]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}))
+}
