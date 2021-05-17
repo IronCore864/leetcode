4f1576e6cb8c3c30deffdf8faceb4b68642b49b2
Tiexin Guo <guotiexin@gmail.com>
2021-05-17 11:37:57 +0200

    116. Populating Next Right Pointers in Each Node - rework, python3, level order traverse

---
+++ 1/116_populating_next_right_pointers_in_each_node/main.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+
+        current_level = [root]
+
+        while current_level:
+            next_level = []
+            for i in range(len(current_level)):
+                if i != len(current_level)-1:
+                    current_level[i].next = current_level[i+1]
+
+                if current_level[i].left:
+                    next_level.append(current_level[i].left)
+                if current_level[i].right:
+                    next_level.append(current_level[i].right)
+            current_level = next_level
+
+        return root
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+
+    s.connect(root)
+    assert root.next == None
+    assert root.left.next == root.right
+    assert root.right.next == None
+    assert root.left.left.next == root.left.right
+    assert root.left.right.next == root.right.left
+    assert root.right.left.next == root.right.right
+    assert root.right.right.next == None
