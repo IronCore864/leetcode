6b9709fa476c8f0648473fc263bbece6c7dec453
Tiexin Guo <guotiexin@gmail.com>
2021-09-13 12:38:33 +0800

    341. Flatten Nested List Iterator - rework, stack

---
+++ 3/341_flatten_nested_list_iterator/main.py
@@ -0,0 +1,24 @@
+class NestedIterator(object):
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack = nestedList[::-1]
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop().getInteger()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.stack:
+            top = self.stack[-1]
+            if top.isInteger():
+                return True
+            self.stack = self.stack[:-1] + top.getList()[::-1]
+        return False
