b06eeaf9489bb8b3415aadc3b15adbcf9bde96b6
Tiexin Guo <guotiexin@gmail.com>
2021-10-21 09:57:38 +0800

    141. Linked List Cycle - rework, two pointers, slow fast, cycle detection

---
+++ 1/141_linked_list_cycle/main.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+
+        # not necessary to test slow because if fast, slow must not be None
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+
+        return False
