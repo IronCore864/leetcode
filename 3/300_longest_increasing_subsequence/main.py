a62a4cdde9814ae48997176debcaad537f7ad01f
Tiexin Guo <guotiexin@gmail.com>
2021-11-03 11:18:09 +0800

    300. Longest Increasing Subsequence - rework, dp

---
+++ 3/300_longest_increasing_subsequence/main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        dp = [1] * N
+
+        for i in range(1, N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+
+        return max(dp)
