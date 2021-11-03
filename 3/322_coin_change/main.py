652007eab9a1a76bc52a6f39bf5b9b3e200e62b0
Tiexin Guo <guotiexin@gmail.com>
2021-11-03 10:12:23 +0800

    322. Coin Change - rework, dp with memory

--- 3/322_coin_change/main.py
+++ 3/322_coin_change/main.py
@@ -4,15 +4,19 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        res = [sys.maxsize] * (amount+1)
-        res[0] = 0
+        dp = [sys.maxsize] * (amount + 1)
+        dp[0] = 0
 
         for i in range(1, amount+1):
             for c in coins:
                 if i >= c:
-                    res[i] = min(res[i], res[i-c] + 1)
+                    dp[i] = min(dp[i], dp[i-c] + 1)
 
-        if res[amount] == sys.maxsize:
-            return -1
+        return dp[-1]if dp[-1] != sys.maxsize else -1
 
-        return res[amount]
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.coinChange([1, 2, 5], 11))
+    print(s.coinChange([2], 3))
+    print(s.coinChange([186, 419, 83, 408], 6249))
