b3ca743aad1cfc42362492cd56a82e9b5657e26a
Tiexin Guo <guotiexin@gmail.com>
2022-11-30 09:22:34 +0800

    22. Generate Parentheses

--- 0/22_generate_parentheses/main.py
+++ 0/22_generate_parentheses/main.py
@@ -1,55 +1,26 @@
+from typing import List
+
+
 class Solution:
-    def is_valid(self, result):
-        balance = 0
-        for c in result:
-            if c == '(':
-                balance += 1
-            else:
-                balance -= 1
-            if balance < 0:
-                return False
-        return balance == 0
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
 
-    def generateParenthesis(self, n: int) -> list[str]:
-        result = []
-
-        def dfs_backtracking(path=''):
-            if len(path) == 2*n:
-                if self.is_valid(path):
-                    result.append(path)
+        def dfs(path, left, right):
+            if len(path) == n*2:
+                res.append(path)
                 return
 
-            path += '('
-            dfs_backtracking(path)
-            path = path[0:len(path)-1]
-
-            path += ')'
-            dfs_backtracking(path)
-
-        def dfs_backtracking_optimized(path='', left=0, right=0):
-            # skip invalid results
-            if len(path) == 2*n:
-                result.append(path)
-                return
-
-            # only add left parenthesis when left < n
-            # for example, if n == 2, ((() will apparently be not valid
             if left < n:
-                path += '('
-                dfs_backtracking_optimized(path, left+1, right)
-                path = path[0:len(path)-1]
-
-            # only add right parenthesis when left >right
-            # for example, if n == 2, ())) will apparently be not valid
+                dfs(path+'(', left+1, right)
             if left > right:
-                path += ')'
-                dfs_backtracking_optimized(path, left, right+1)
+                dfs(path+')', left, right+1)
 
-        # dfs_backtracking()
-        dfs_backtracking_optimized()
-        return result
+        dfs('', 0, 0)
+
+        return res
 
 
 if __name__ == '__main__':
     s = Solution()
+    print(s.generateParenthesis(1))
     print(s.generateParenthesis(3))
